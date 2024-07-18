bc66b5148dc7e0d8c5bac0161628697c97dbf4b7
Frank Gehann <frank.gehann@bechtle.com>
2024-07-18 11:26:25 +0200

    Added abillity to resolve from an IConfiguration in addition to the IConfigurationRoot

--- src/AspNetCore.Cors.Extensions/CorsPolicyExtensions.cs
+++ src/AspNetCore.Cors.Extensions/CorsPolicyExtensions.cs
@@ -10,13 +10,23 @@ namespace AspNetCore.Cors.Extensions;
 public static class CorsPolicyExtensions
 {
     /// <summary>
-    /// Adds cors policies configured in the default section name "Cors"
+    /// Adds cors policies configured in the default section name "Cors" by resolving from a <see cref="IConfigurationRoot"/>
     /// </summary>
     /// <param name="serviceCollection">service collection used to add the policy to</param>
     /// <param name="configurationRoot">the <see cref="IConfigurationRoot"/> used to resolve the configuration</param>
     public static void AddCorsPolicies(this IServiceCollection serviceCollection, IConfigurationRoot configurationRoot)
     {
         serviceCollection.AddCorsPolicies(configurationRoot.GetSection("Cors"));
+    }
+    
+    /// <summary>
+    /// Adds cors policies configured in the default section name "Cors" by resolving from a <see cref="IConfiguration"/>
+    /// </summary>
+    /// <param name="serviceCollection">service collection used to add the policy to</param>
+    /// <param name="configuration">the <see cref="IConfiguration"/> used to resolve the configuration</param>
+    public static void AddCorsPolicies(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        serviceCollection.AddCorsPolicies(configuration.GetSection("Cors"));
     }
 
     /// <summary>
